186525658cee7a8be19e850ab169df8920d43cb8
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-03-18 09:47:49 +0530

    feat: understood how anyhow works (in brief)

--- anyhow-sandbox/src/main.rs
+++ anyhow-sandbox/src/main.rs
@@ -1,14 +1,47 @@
-use anyhow::Result;
-use std::fs;
+use anyhow::{Context, Result};
+use std::fmt;
 
-fn read_number() -> Result<i32> {
-    let s = fs::read_to_string("num.txt")?;
-    let n: i32 = s.trim().parse()?;
-    Ok(n)
+#[derive(Debug)]
+struct ConfigError;
+impl fmt::Display for ConfigError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "configuration file is missing")
+    }
+}
+impl std::error::Error for ConfigError {}
+
+#[derive(Debug)]
+struct NetworkError(u32);
+impl fmt::Display for NetworkError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "connection failed with status: {}", self.0)
+    }
+}
+impl std::error::Error for NetworkError {}
+
+fn load_config() -> Result<(), ConfigError> {
+    Err(ConfigError)
 }
 
-fn main() -> Result<()> {
-    let n = read_number()?;
-    println!("number: {}", n);
-    Ok(())
+fn connect_to_server() -> Result<(), NetworkError> {
+    Err(NetworkError(500))
+}
+
+fn run_app() -> Result<u32> {
+    connect_to_server()?;
+    load_config()?;
+    // connect_to_server().context("Failed to reach API")?;
+    // load_config().context("Failed to initialize system")?;
+
+    Ok(0)
+}
+
+fn main() {
+    if let Err(e) = run_app() {
+        // Print the "root cause" and the "context chain"
+        // eprintln!("Error: {}", e);
+
+        eprintln!("Errors ---");
+        println!("{:?}", e);
+    }
 }
